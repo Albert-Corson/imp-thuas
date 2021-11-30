1c1190022252d28360cb3a19dae00b0ad16f985e
Albert Corson <albert.corson@epitech.eu>
2021-11-30 12:52:26 +0100

    feat: learnig lab notebook

--- config/__init__.py
+++ config/__init__.py
@@ -1,10 +1,8 @@
-from .knmi import config as knmi_config
 from .factory_zero import config as fz_config
 
 config = {
     "upload_dir": "./data/",
     "datasets_configs": {
-        "factory zero": fz_config,
-        "knmi": knmi_config
+        "factory zero": fz_config
     }
 }
